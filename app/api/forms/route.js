4258386859d390c57853b2b25d8939d7eede79bf
rajmahadev422 <rajmahadev422@gmail.com>
2026-05-30 22:34:46 +0530

    fix form error

--- app/api/forms/route.js
+++ app/api/forms/route.js
@@ -24,11 +24,13 @@ export async function POST(req) {
   const userEmail = req.headers.get('x-user-email');
 
   if(!userEmail) return NextResponse.json({error: "User not found"}, {status: 404});
+  console.log(userEmail)
   try {
     await connectDB();
-    const body = await req.json();
+    let body = await req.json();
+    body = {...body, userEmail};
     const form = await Form.create(body);
-    return NextResponse.json({ ...form, userEmail }, { status: 201 });
+    return NextResponse.json(form, { status: 201 });
   } catch {
     return NextResponse.json(
       { error: "Failed to create form" },
