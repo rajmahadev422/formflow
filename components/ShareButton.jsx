267bf25e1ac64a84ef6ca5277571b503f7b80585
rajmahadev422 <rajmahadev422@gmail.com>
2026-05-30 23:33:17 +0530

    fix the share button icon

--- components/ShareButton.jsx
+++ components/ShareButton.jsx
@@ -40,24 +40,7 @@ export default function ShareButton({ formId, title, desc }) {
       {copied ? (
         <span>✔</span>
       ) : (
-        <svg
-          fill="#000000"
-          viewBox="-2 -2 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="xMinYMin"
-          className="jam jam-share-alt h-20 w-20"
-
-        >
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-          <g
-            id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></g>
-          <g id="SVGRepo_iconCarrier">
-            <path d="M16 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4zM7.928 9.24a4.02 4.02 0 0 1-.026 1.644l5.04 2.537a4 4 0 1 1-.867 1.803l-5.09-2.562a4 4 0 1 1 .083-5.228l5.036-2.522a4 4 0 1 1 .929 1.772L7.928 9.24zM4 12a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm12 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"></path>
-          </g>
-        </svg>
+        <span>જ⁀➴</span>
       )}
     </button>
   );
